b8622798209cf5c460c98d3b97ffe23f88aa96b2
Will Curtis <wcurtisusa@gmail.com>
2018-04-17 04:11:24 -0400

    Layout work

---
+++ app/src/main/java/com/example/giftlistguru/DisplayRecipient.java
@@ -0,0 +1,57 @@
+package com.example.giftlistguru;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+/**
+ * Created by willi on 4/17/2018.
+ */
+
+public class DisplayRecipient extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipient);
+        // set up toolbar
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+    }
+
+    // creates menu in Appbar
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        Intent i = new Intent(DisplayRecipient.this, EditRecipient.class);
+        Intent j = new Intent(DisplayRecipient.this, MainActivity.class);
+
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                startActivity(j);
+                return true;
+            case R.id.editButton:
+                startActivity(i);
+                return true;
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+
+
+}
--- app/src/main/java/com/example/giftlistguru/MainActivity.java
+++ app/src/main/java/com/example/giftlistguru/MainActivity.java
@@ -1,7 +1,14 @@
 package com.example.giftlistguru;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,42 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // set up toolbar
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+    }
+
+    // creates menu in Appbar
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu3, menu);
+        return true;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        final Spinner recipientSpinner = (Spinner) findViewById(R.id.recipientSpinner);
+
+        recipientSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if (recipientSpinner.getSelectedItemPosition() != 0) {
+                    Intent i = new Intent(MainActivity.this, DisplayRecipient.class);
+                    startActivityForResult(i, 1);
+                }
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
     }
 }
+
+
